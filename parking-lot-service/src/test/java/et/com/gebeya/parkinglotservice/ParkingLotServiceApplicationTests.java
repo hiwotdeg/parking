a15a09ddf01cabbdb414cbb67cb305db45c49929
Abinet <abinettamiru28@gmail.com>
2024-02-05 23:54:42 +0300

    feat: add parking lot service

---
+++ parking-lot-service/src/test/java/et/com/gebeya/parkinglotservice/ParkingLotServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package et.com.gebeya.parkinglotservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingLotServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
