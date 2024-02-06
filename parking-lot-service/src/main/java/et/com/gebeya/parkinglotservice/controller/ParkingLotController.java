940f08322373ef0bb0966d4c9b434a74a60c9235
Kinn7 <kidus.berhanu.gh9353@gmail.com>
2024-02-06 22:50:06 +0300

    feat: add controller for RegistrationService

--- parking-lot-service/src/main/java/et/com/gebeya/parkinglotservice/controller/ParkingLotController.java
+++ parking-lot-service/src/main/java/et/com/gebeya/parkinglotservice/controller/ParkingLotController.java
@@ -1,16 +1,24 @@
 package et.com.gebeya.parkinglotservice.controller;
 
+import et.com.gebeya.parkinglotservice.dto.AddUserResponse;
+import et.com.gebeya.parkinglotservice.model.ParkingLotProvider;
+import et.com.gebeya.parkinglotservice.service.RegistrationService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/parking-lot")
+@RequiredArgsConstructor
 public class ParkingLotController {
-
+    private final RegistrationService registrationService;
     @GetMapping("/hello")
     public ResponseEntity<?> hello(){
         return ResponseEntity.ok("hello");
+    }
+
+    @PostMapping("/register/lot-provider")
+    public ResponseEntity<AddUserResponse> registerParkingLotProvider(@RequestBody ParkingLotProvider parkingLotProvider){
+        return ResponseEntity.ok(registrationService.registerParkingLotProvider(parkingLotProvider));
     }
 }
