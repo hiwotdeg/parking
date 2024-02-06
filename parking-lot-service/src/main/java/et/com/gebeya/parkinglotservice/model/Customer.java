e17d88456929548e6ff98aad7ed42cc1b44d4e4b
Kinn7 <kidus.berhanu.gh9353@gmail.com>
2024-02-06 19:48:14 +0300

    feat: add customer model and customer repository

--- parking-lot-service/src/main/java/et/com/gebeya/parkinglotservice/model/Customer.java
+++ parking-lot-service/src/main/java/et/com/gebeya/parkinglotservice/model/Customer.java
@@ -1,2 +1,23 @@
-package et.com.gebeya.parkinglotservice.model;public class Customer {
+package et.com.gebeya.parkinglotservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "customer")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNo;
+    private Boolean isActive;
+    private String imageUrl;
 }
