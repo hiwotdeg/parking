75f088f2d16eaeea4be9033a26bc749100879813
Kinn7 <kidus.berhanu.gh9353@gmail.com>
2024-02-06 22:50:54 +0300

    feat: add dto for AddUserRequest and AddUserResponse

---
+++ parking-lot-service/src/main/java/et/com/gebeya/parkinglotservice/dto/AddUserRequest.java
@@ -0,0 +1,17 @@
+package et.com.gebeya.parkinglotservice.dto;
+
+import et.com.gebeya.parkinglotservice.enums.Authority;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class AddUserRequest {
+    private String phoneNo;
+    private Authority role;
+    private Integer roleId;
+}
--- parking-lot-service/src/main/java/et/com/gebeya/parkinglotservice/dto/AddUserResponse.java
+++ parking-lot-service/src/main/java/et/com/gebeya/parkinglotservice/dto/AddUserResponse.java
@@ -1,2 +1,14 @@
-package et.com.gebeya.parkinglotservice.dto;public class AddUserResponse {
+package et.com.gebeya.parkinglotservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AddUserResponse {
+    private String token;
 }
