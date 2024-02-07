54725e93a503d6a5d58e37d841248de44513f90f
Kinn7 <kidus.berhanu.gh9353@gmail.com>
2024-02-07 14:17:05 +0300

    feat: add controller for customer and register customer

--- parking-lot-service/src/main/java/et/com/gebeya/parkinglotservice/service/ParkingLotProviderService.java
+++ parking-lot-service/src/main/java/et/com/gebeya/parkinglotservice/service/ParkingLotProviderService.java
@@ -2,7 +2,6 @@ package et.com.gebeya.parkinglotservice.service;
 
 import et.com.gebeya.parkinglotservice.dto.AddUserRequest;
 import et.com.gebeya.parkinglotservice.dto.AddUserResponse;
-import et.com.gebeya.parkinglotservice.model.Customer;
 import et.com.gebeya.parkinglotservice.model.ParkingLotProvider;
 import et.com.gebeya.parkinglotservice.repository.CustomerRepository;
 import et.com.gebeya.parkinglotservice.repository.ParkingLotProviderRepository;
@@ -13,11 +12,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
-import java.util.Objects;
-
 @Service
 @RequiredArgsConstructor
-public class RegistrationService {
+public class ParkingLotProviderService {
     private final WebClient.Builder webClientBuilder;
     private final CustomerRepository customerRepository;
     private final ParkingLotProviderRepository parkingLotProviderRepository;
