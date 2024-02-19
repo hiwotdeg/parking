c28e80030cb81a24f8308b75b25a12c6571e1e2c
Abinet <abinettamiru28@gmail.com>
2024-02-19 13:49:45 +0300

    feat: add swagger in parking-lot-service

--- parking-lot-service/src/main/java/et/com/gebeya/parkinglotservice/config/SecurityConfig.java
+++ parking-lot-service/src/main/java/et/com/gebeya/parkinglotservice/config/SecurityConfig.java
@@ -34,10 +34,16 @@ public class SecurityConfig {
     };
 
     protected static final String [] SWAGGER_MATCHERS = {
-            "/swagger-ui/**",
+            "/v2/api-docs",
+            "/v3/api-docs",
             "/v3/api-docs/**",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui/**",
+            "/webjars/**",
             "/swagger-ui.html",
-            "/swagger-ui.html/**",
             "/actuator",
             "/actuator/**"
     };
