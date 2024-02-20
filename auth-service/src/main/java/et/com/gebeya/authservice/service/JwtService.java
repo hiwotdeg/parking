7b72f7c2549e272a73897a63244c1a9e94f3dbb6
Abinet <abinettamiru28@gmail.com>
2024-02-20 13:13:11 +0300

    fix: token expiration time increased to 24 hours

--- auth-service/src/main/java/et/com/gebeya/authservice/service/JwtService.java
+++ auth-service/src/main/java/et/com/gebeya/authservice/service/JwtService.java
@@ -56,7 +56,7 @@ public class JwtService {
         return Jwts.builder().setClaims(extraClaims)
                 .setSubject(users.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60* 60))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60* 60 * 24))
                 .signWith(getSigningKey(), SignatureAlgorithm.HS256).compact();
     }
 
