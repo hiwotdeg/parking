6498ffa045d0b6bbedded079e5de48663a578c56
Abinet <abinettamiru28@gmail.com>
2024-02-20 23:40:34 +0300

    feat: add security feature for geo-location-service

--- geo-location-service/src/main/java/et/com/gebeya/geolocationservice/controller/LocationController.java
+++ geo-location-service/src/main/java/et/com/gebeya/geolocationservice/controller/LocationController.java
@@ -4,16 +4,14 @@ import et.com.gebeya.geolocationservice.dto.GetLocationRequest;
 import et.com.gebeya.geolocationservice.service.LocationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import redis.clients.jedis.resps.GeoRadiusResponse;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/geo-location")
 public class LocationController {
     private final LocationService locationService;
 
@@ -28,7 +26,7 @@ public class LocationController {
 //        return ResponseEntity.ok("");
 //    }
 
-    @PostMapping("/getLocation")
+    @PostMapping("/get-parking-lots")
     public ResponseEntity<List<GeoRadiusResponse>> getLocation(@RequestBody GetLocationRequest request){
         return ResponseEntity.ok(locationService.getPointsWithinRadius(request));
     }
---
+++ geo-location-service/src/main/java/et/com/gebeya/geolocationservice/security/RoleHeaderAuthenticationToken.java
@@ -0,0 +1,33 @@
+package et.com.gebeya.geolocationservice.security;
+
+import lombok.Getter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+
+@Getter
+public class RoleHeaderAuthenticationToken extends AbstractAuthenticationToken {
+    private String headerRole;
+    private Integer roleId;
+
+    public RoleHeaderAuthenticationToken(String headerRole, String roleId) {
+        super(null);
+
+        this.headerRole = headerRole;
+        assert roleId != null;
+        this.roleId = Integer.valueOf(roleId);
+        setAuthenticated(false);
+
+
+    }
+
+    @Override
+    public Object getCredentials() {
+        return null;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return headerRole;
+    }
+
+    // Getters for headerRole and authorities
+}
