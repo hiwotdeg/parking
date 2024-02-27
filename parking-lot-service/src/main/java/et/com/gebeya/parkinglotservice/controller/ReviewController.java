d418abf5f576c97e3eae429d73ec7e4ba30abbfa
Kinn7 <kidus.berhanu.gh9353@gmail.com>
2024-02-27 19:08:45 +0300

    feat: add function for accepting and responding to updating a review

--- parking-lot-service/src/main/java/et/com/gebeya/parkinglotservice/controller/ReviewController.java
+++ parking-lot-service/src/main/java/et/com/gebeya/parkinglotservice/controller/ReviewController.java
@@ -1,6 +1,8 @@
 package et.com.gebeya.parkinglotservice.controller;
 
 import et.com.gebeya.parkinglotservice.dto.requestdto.AddReviewRequestDto;
+import et.com.gebeya.parkinglotservice.dto.requestdto.UpdateReviewRequestDto;
+import et.com.gebeya.parkinglotservice.dto.responsedto.ReviewResponseDto;
 import et.com.gebeya.parkinglotservice.service.ReviewService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +15,12 @@ public class ReviewController {
     private final ReviewService reviewService;
 
     @PostMapping("/reviews")
-    public ResponseEntity<?> addReview(@RequestBody AddReviewRequestDto addReviewRequestDto){
+    public ResponseEntity<ReviewResponseDto> addReview(@RequestBody AddReviewRequestDto addReviewRequestDto){
         return ResponseEntity.ok(reviewService.createReviewForParkingLot(addReviewRequestDto));
+    }
+
+    @PatchMapping("/reviews")
+    public ResponseEntity<ReviewResponseDto> updateReview(@RequestBody UpdateReviewRequestDto updateReviewRequestDto){
+        return ResponseEntity.ok(reviewService.updateReviewForParkingLot(updateReviewRequestDto));
     }
 }
