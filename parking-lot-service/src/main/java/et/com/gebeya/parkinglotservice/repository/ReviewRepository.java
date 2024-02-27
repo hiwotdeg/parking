b14b02a2fa9f74eec8ed33f77dcc0c9ec4a9aaaf
Kinn7 <kidus.berhanu.gh9353@gmail.com>
2024-02-27 21:12:01 +0300

    feat: select average rate based on parking lot id

--- parking-lot-service/src/main/java/et/com/gebeya/parkinglotservice/repository/ReviewRepository.java
+++ parking-lot-service/src/main/java/et/com/gebeya/parkinglotservice/repository/ReviewRepository.java
@@ -3,9 +3,12 @@ package et.com.gebeya.parkinglotservice.repository;
 import et.com.gebeya.parkinglotservice.model.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review,Integer>, JpaSpecificationExecutor<Review> {
-
+    @Query("SELECT AVG(r.rate) FROM Review r WHERE r.parkingLot.id = :parkingLotId")
+    Float calculateAverageRatingByParkingLotId(@Param("parkingLotId") Integer parkingLotId);
 }
