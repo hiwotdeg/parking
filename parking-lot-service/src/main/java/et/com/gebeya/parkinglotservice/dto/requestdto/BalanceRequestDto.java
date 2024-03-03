743e0e546feab39155f82f5c98a1acf62bb29ae2
Abinet <abinettamiru28@gmail.com>
2024-03-04 00:14:22 +0300

    feat: create balance during the creating of driver

---
+++ parking-lot-service/src/main/java/et/com/gebeya/parkinglotservice/dto/requestdto/BalanceRequestDto.java
@@ -0,0 +1,17 @@
+package et.com.gebeya.parkinglotservice.dto.requestdto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class BalanceRequestDto {
+    private Integer userId;
+    private BigDecimal amount;
+}
--- parking-lot-service/src/main/java/et/com/gebeya/parkinglotservice/service/DriverService.java
+++ parking-lot-service/src/main/java/et/com/gebeya/parkinglotservice/service/DriverService.java
@@ -2,8 +2,10 @@ package et.com.gebeya.parkinglotservice.service;
 
 import et.com.gebeya.parkinglotservice.dto.requestdto.AddDriverRequestDto;
 import et.com.gebeya.parkinglotservice.dto.requestdto.AddUserRequest;
+import et.com.gebeya.parkinglotservice.dto.requestdto.BalanceRequestDto;
 import et.com.gebeya.parkinglotservice.dto.requestdto.UpdateDriverRequestDto;
 import et.com.gebeya.parkinglotservice.dto.responsedto.AddUserResponse;
+import et.com.gebeya.parkinglotservice.dto.responsedto.BalanceResponseDto;
 import et.com.gebeya.parkinglotservice.dto.responsedto.DriverResponseDto;
 import et.com.gebeya.parkinglotservice.exception.AuthException;
 import et.com.gebeya.parkinglotservice.exception.DriverIdNotFound;
@@ -13,16 +15,19 @@ import et.com.gebeya.parkinglotservice.repository.specification.DriverSpecificat
 import et.com.gebeya.parkinglotservice.util.MappingUtil;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class DriverService {
     private final WebClient.Builder webClientBuilder;
     private final DriverRepository driverRepository;
@@ -40,6 +45,9 @@ public class DriverService {
                 .retrieve()
                 .toEntity(AddUserResponse.class);
 
+        BalanceRequestDto requestDto = BalanceRequestDto.builder().userId(driver.getId()).amount(BigDecimal.valueOf(0.0)).build();
+        BalanceResponseDto responseDto = createBalance(requestDto);
+        log.info("Response from Payment micro service==> {}", responseDto);
         return responseMono.blockOptional()
                 .map(ResponseEntity::getBody)
                 .orElseThrow(() -> new AuthException("Error occurred during generating of token"));
@@ -66,5 +74,15 @@ public class DriverService {
         if (driver.isEmpty())
             throw new DriverIdNotFound("Driver is not found");
         return driver.get(0);
+    }
+
+    private BalanceResponseDto createBalance(BalanceRequestDto balanceRequestDto){
+        return webClientBuilder.build().post()
+                .uri("http://PAYMENT-SERVICE/api/v1/payment/driver")
+                .bodyValue(balanceRequestDto)
+                .retrieve()
+                .bodyToMono(BalanceResponseDto.class)
+                .block();
+
     }
 }
