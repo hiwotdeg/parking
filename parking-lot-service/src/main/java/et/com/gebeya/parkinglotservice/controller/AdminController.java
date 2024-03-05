9a963cb0671dc2a7fe0ec20ded1334769bd478b9
Kinn7 <kidus.berhanu.gh9353@gmail.com>
2024-03-05 19:17:43 +0300

    feat: add admin controller template

---
+++ parking-lot-service/src/main/java/et/com/gebeya/parkinglotservice/controller/AdminController.java
@@ -0,0 +1,11 @@
+package et.com.gebeya.parkinglotservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/parking-lot")
+public class AdminController {
+
+}
