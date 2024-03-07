23d45895a42a8f5e77c0d5e0fdf93f06675057cf
Abinet <abinettamiru28@gmail.com>
2024-03-08 01:26:31 +0300

    feat: add controller for reservation confirm

--- parking-lot-service/src/main/java/et/com/gebeya/parkinglotservice/controller/ReservationController.java
+++ parking-lot-service/src/main/java/et/com/gebeya/parkinglotservice/controller/ReservationController.java
@@ -1,6 +1,7 @@
 package et.com.gebeya.parkinglotservice.controller;
 
 import et.com.gebeya.parkinglotservice.dto.requestdto.ReservationRequestDto;
+import et.com.gebeya.parkinglotservice.dto.requestdto.UpdateReservation;
 import et.com.gebeya.parkinglotservice.dto.responsedto.ReservationResponseDto;
 import et.com.gebeya.parkinglotservice.model.Reservation;
 import et.com.gebeya.parkinglotservice.service.ReservationService;
@@ -8,7 +9,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalTime;
 import java.util.List;
 import java.util.Map;
 
@@ -21,6 +21,11 @@ public class ReservationController {
     @PostMapping("/lots/{parkingLotId}/reservations")
     public ResponseEntity<Map<String,String>> book(@RequestBody ReservationRequestDto requestDto, @PathVariable("parkingLotId") Integer parkingLotId){
         return ResponseEntity.ok(reservationService.book(parkingLotId,ReservationRequestDto.builder().stayingDuration(requestDto.getStayingDuration()).build()));
+    }
+
+    @PostMapping("/reservations/{reservationId}/requests")
+    public ResponseEntity<ReservationResponseDto> updateStatus(@PathVariable("reservationId") Integer reservationId,@RequestBody UpdateReservation dto){
+        return ResponseEntity.ok(reservationService.updateReservation(reservationId,dto));
     }
 
     @GetMapping("/lots/reservations/")
--- parking-lot-service/src/main/java/et/com/gebeya/parkinglotservice/dto/requestdto/UpdateReservation.java
+++ parking-lot-service/src/main/java/et/com/gebeya/parkinglotservice/dto/requestdto/UpdateReservation.java
@@ -1,2 +1,15 @@
-package et.com.gebeya.parkinglotservice.dto.requestdto;public class UpdateReservation {
+package et.com.gebeya.parkinglotservice.dto.requestdto;
+
+import et.com.gebeya.parkinglotservice.enums.ReservationStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class UpdateReservation {
+    private ReservationStatus reservationStatus;
 }
