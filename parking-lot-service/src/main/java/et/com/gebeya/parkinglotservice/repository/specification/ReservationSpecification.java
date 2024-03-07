08be240a15753d8144094dbaf4683c21a01a135e
Abinet <abinettamiru28@gmail.com>
2024-03-08 01:14:36 +0300

    feat: add find reservation by reservationid

--- parking-lot-service/src/main/java/et/com/gebeya/parkinglotservice/repository/specification/ReservationSpecification.java
+++ parking-lot-service/src/main/java/et/com/gebeya/parkinglotservice/repository/specification/ReservationSpecification.java
@@ -4,6 +4,7 @@ import et.com.gebeya.parkinglotservice.model.ParkingLot;
 import et.com.gebeya.parkinglotservice.model.ParkingLotProvider;
 import et.com.gebeya.parkinglotservice.model.Reservation;
 import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.Predicate;
 import org.springframework.data.jpa.domain.Specification;
 
 public class ReservationSpecification {
@@ -12,6 +13,14 @@ public class ReservationSpecification {
             Join<Reservation, ParkingLot> parkingLotJoin = root.join("parkingLot");
             Join<ParkingLot, ParkingLotProvider> providerJoin = parkingLotJoin.join("parkingLotProvider");
             return criteriaBuilder.equal(providerJoin.get("id"), providerId);
+        };
+    }
+
+    public static Specification<Reservation> getReservationById(Integer id) {
+        return (root, query, criteriaBuilder) -> {
+            Predicate isActive = criteriaBuilder.notEqual(root.get("isActive"), false);
+            Predicate isReservation = criteriaBuilder.equal(root.get("id"), id);
+            return criteriaBuilder.and(isActive, isReservation);
         };
     }
 }
