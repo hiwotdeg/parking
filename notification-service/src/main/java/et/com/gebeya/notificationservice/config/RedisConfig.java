6b07cf38c2adcac792aa9a6865545614a0b29ba2
Abinet <abinettamiru28@gmail.com>
2024-03-17 00:15:26 +0300

    fix: environmental variable for redis in notification service

--- notification-service/src/main/java/et/com/gebeya/notificationservice/config/RedisConfig.java
+++ notification-service/src/main/java/et/com/gebeya/notificationservice/config/RedisConfig.java
@@ -1,8 +1,10 @@
 package et.com.gebeya.notificationservice.config;
 
 import et.com.gebeya.notificationservice.dto.MessageDto;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
@@ -12,11 +14,12 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
-
+    @Value("${redishost}")
+    private String redisHost;
 
     @Bean
-    JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
+    public JedisConnectionFactory jedisConnectionFactory() {
+        return new JedisConnectionFactory(new RedisStandaloneConfiguration(redisHost, 6379));
     }
 
     @Bean
--- payment-service/src/main/java/et/com/gebeya/paymentservice/PaymentServiceApplication.java
+++ payment-service/src/main/java/et/com/gebeya/paymentservice/PaymentServiceApplication.java
@@ -3,6 +3,7 @@ package et.com.gebeya.paymentservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
