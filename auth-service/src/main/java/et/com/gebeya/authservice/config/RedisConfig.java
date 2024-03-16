8d0fb9c3695bf402784370bd9ccac106dc2f439e
Abinet <abinettamiru28@gmail.com>
2024-03-17 00:16:17 +0300

    fix: environmental variable for redis in auth service

--- auth-service/src/main/java/et/com/gebeya/authservice/config/RedisConfig.java
+++ auth-service/src/main/java/et/com/gebeya/authservice/config/RedisConfig.java
@@ -1,7 +1,9 @@
 package et.com.gebeya.authservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -10,11 +12,13 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+    @Value("${redishost}")
+    private String redisHost;
 
 
     @Bean
-    JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
+    public JedisConnectionFactory jedisConnectionFactory() {
+        return new JedisConnectionFactory(new RedisStandaloneConfiguration(redisHost, 6379));
     }
 
     @Bean
