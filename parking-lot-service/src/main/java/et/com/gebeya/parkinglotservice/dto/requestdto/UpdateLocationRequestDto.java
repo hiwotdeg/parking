06784b9848cdd00bb904b7e02a5aa1a96ef47336
Abinet <abinettamiru28@gmail.com>
2024-03-17 14:46:30 +0300

    feat: add update location producer

--- parking-lot-service/src/main/java/et/com/gebeya/parkinglotservice/dto/requestdto/UpdateLocationRequestDto.java
+++ parking-lot-service/src/main/java/et/com/gebeya/parkinglotservice/dto/requestdto/UpdateLocationRequestDto.java
@@ -1,2 +1,20 @@
-package et.com.gebeya.parkinglotservice.dto.requestdto;public class UpdateLocationRequestDto {
+package et.com.gebeya.parkinglotservice.dto.requestdto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class UpdateLocationRequestDto {
+    private Integer id;
+    private String existingTitle;
+    private String existingAddress;
+    private String newTitle;
+    private String newAddress;
+    private Double latitude;
+    private Double longitude;
 }
