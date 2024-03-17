b44189d5a9611ca970dc1027a5b417e4e5ce4eda
Abinet <abinettamiru28@gmail.com>
2024-03-17 15:38:19 +0300

    feat: add consumer and the logic to update location

--- geo-location-service/src/main/java/et/com/gebeya/geolocationservice/listeners/KafkaListeners.java
+++ geo-location-service/src/main/java/et/com/gebeya/geolocationservice/listeners/KafkaListeners.java
@@ -2,14 +2,14 @@ package et.com.gebeya.geolocationservice.listeners;
 
 import et.com.gebeya.geolocationservice.dto.AddLocationDto;
 import et.com.gebeya.geolocationservice.dto.DeleteLocationDto;
+import et.com.gebeya.geolocationservice.dto.UpdateLocationDto;
 import et.com.gebeya.geolocationservice.service.LocationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
-import static et.com.gebeya.geolocationservice.util.Constant.ADD_LOCATION_TOPIC;
-import static et.com.gebeya.geolocationservice.util.Constant.DELETE_LOCATION_TOPIC;
+import static et.com.gebeya.geolocationservice.util.Constant.*;
 
 @Component
 @RequiredArgsConstructor
@@ -23,6 +23,12 @@ private final LocationService locationService;
     void addLocationDtoListener(AddLocationDto dto) {
         log.info(dto.toString());
         locationService.addLocation(dto);
+    }
+
+    @KafkaListener(topics = UPDATE_LOCATION_TOPIC, groupId = "group7", containerFactory = "updateLocationListenerFactory")
+    void updateLocationDtoListener(UpdateLocationDto dto) {
+        log.info(dto.toString());
+        locationService.updateLocation(dto);
     }
 
     @KafkaListener(topics = DELETE_LOCATION_TOPIC, groupId = "group3", containerFactory = "deleteLocationListenerFactory")
--- geo-location-service/src/main/java/et/com/gebeya/geolocationservice/service/LocationService.java
+++ geo-location-service/src/main/java/et/com/gebeya/geolocationservice/service/LocationService.java
@@ -1,10 +1,7 @@
 package et.com.gebeya.geolocationservice.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import et.com.gebeya.geolocationservice.dto.AddLocationDto;
-import et.com.gebeya.geolocationservice.dto.DeleteLocationDto;
-import et.com.gebeya.geolocationservice.dto.GetLocationRequest;
-import et.com.gebeya.geolocationservice.dto.LocationDto;
+import et.com.gebeya.geolocationservice.dto.*;
 import et.com.gebeya.geolocationservice.exception.InvalidLocationException;
 import et.com.gebeya.geolocationservice.util.MappingUtil;
 import lombok.RequiredArgsConstructor;
@@ -26,38 +23,52 @@ import java.util.Map;
 @RequiredArgsConstructor
 
 public class LocationService {
-   private final Jedis jedis;
-   private final ObjectMapper objectMapper;
+    private final Jedis jedis;
+    private final ObjectMapper objectMapper;
+
     @SneakyThrows
     public void addLocation(AddLocationDto addLocationDto) {
-        Map<String,Object> member = new HashMap<>();
-        member.put("id",addLocationDto.getId());
-        member.put("title",addLocationDto.getTitle());
-        member.put("address",addLocationDto.getAddress());
+        Map<String, Object> member = new HashMap<>();
+        member.put("id", addLocationDto.getId());
+        member.put("title", addLocationDto.getTitle());
+        member.put("address", addLocationDto.getAddress());
         String memberString = objectMapper.writeValueAsString(member);
         jedis.geoadd("location", addLocationDto.getLongitude(), addLocationDto.getLatitude(), memberString);
     }
 
+    @SneakyThrows
+    public void updateLocation(UpdateLocationDto updateLocationDto) {
+        deleteLocation(DeleteLocationDto.builder()
+                .id(updateLocationDto.getId())
+                .address(updateLocationDto.getExistingAddress())
+                .title(updateLocationDto.getExistingTitle()).build());
+        addLocation(AddLocationDto.builder()
+                .id(updateLocationDto.getId())
+                .title(updateLocationDto.getNewTitle())
+                .address(updateLocationDto.getNewAddress())
+                .longitude(updateLocationDto.getLongitude())
+                .latitude(updateLocationDto.getLatitude()).build());
+    }
+
     public List<LocationDto> getPointsWithinRadius(GetLocationRequest request) {
-        try{
-            List<GeoRadiusResponse> responses = jedis.georadius("location", request.getLongitude(), request.getLatitude(), 10, GeoUnit.KM,  GeoRadiusParam.geoRadiusParam().withDist().withCoord()  );
+        try {
+            List<GeoRadiusResponse> responses = jedis.georadius("location", request.getLongitude(), request.getLatitude(), 10, GeoUnit.KM, GeoRadiusParam.geoRadiusParam().withDist().withCoord());
             return MappingUtil.mapListOfGeoRadiusResponseToListOfLocationDto(responses);
-        }
-        catch (JedisDataException exception){
+        } catch (JedisDataException exception) {
             throw new InvalidLocationException(exception.getMessage());
         }
 
     }
-    @SneakyThrows
-    public void deleteLocation(DeleteLocationDto deleteLocationDto){
-        Map<String,Object> member = new HashMap<>();
-        member.put("id",deleteLocationDto.getId());
-        member.put("title",deleteLocationDto.getTitle());
-        member.put("address",deleteLocationDto.getAddress());
-        String memberString = objectMapper.writeValueAsString(member);
-        jedis.zrem("location",memberString);
-    }
 
+    @SneakyThrows
+    public void deleteLocation(DeleteLocationDto deleteLocationDto) {
+        Map<String, Object> member = new HashMap<>();
+        member.put("id", deleteLocationDto.getId());
+        member.put("title", deleteLocationDto.getTitle());
+        member.put("address", deleteLocationDto.getAddress());
+        String memberString = objectMapper.writeValueAsString(member);
+        jedis.zrem("location", memberString);
+    }
 
 
 }
